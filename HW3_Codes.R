ec9f70e9af2e340ff3f638b2568d21ba2281e580
ALP TURECI <atureci@fb.com>
2020-03-15 15:40:18 -0400

    HW3 - Q2 %50, Q1 %100

--- HW3_Codes.R
+++ HW3_Codes.R
@@ -1,31 +1,70 @@
+rm(list=ls())	
 
 #Question 1
+
 setwd('/Users/atureci/Documents/ITU/MachineLearning/Week3/HW3-AlpTureci-95583')
 
+library(ISLR)		
+set.seed(pi)
+
 wine_data<-read.csv("winequality-red.csv",header = TRUE, sep=";")
-wine_train<-wine_data[1:1400,]
+scaled_wine_data<-cbind(scale(wine_data))
+wine_train<-scaled_wine_data[1:1400,]
 x_wine_train<-wine_train[,1:11]
 y_wine_train<-wine_train[,12]
 
-wine_test<-wine_data[1401:dim(wine_data)[1],]
+wine_test<-scaled_wine_data[1401:dim(scaled_wine_data)[1],]
 x_wine_test<-wine_test[,1:11]
 y_wine_test<-wine_test[,12]
 
 #Don’t forget to scale each column before you create the model.??
-lm_wine_train<-lm(y_wine~., data = x_wine)
+lm_wine_train<-lm(y_wine_train~., data = as.data.frame(x_wine_train))
 
 #How well did the model predict the results for the last 199 observations?
-wine_test_fit<-predict(lm_wine_train, newdata = x_wine_test)
+wine_test_fit<-predict(lm_wine_train, newdata = as.data.frame(x_wine_test))
 dY<-y_wine_test - wine_test_fit
 testErr <- sqrt(sum(dY*dY))/(length(y_wine_test))	
 
 #What measure did you use to evaluate how well the model did this prediction?
 
 #Next use the model to predict the results for the whole data set and measure how well your model worked. (hint: use the r function lm and the regression example from class)
-wine_data_x<-wine_data[,1:11]
-wine_data_y<-wine_data[,12]
+wine_data_x<-scaled_wine_data[,1:11]
+wine_data_y<-scaled_wine_data[,12]
 
-wine_data_fit<-predict(lm_wine_train, newdata = wine_data_x)
-dYData<-wine_data_y-wine_data_fit
+lm_wine_data<-predict(lm_wine_train, newdata = as.data.frame(wine_data_x))
+dYData<-wine_data_y - lm_wine_data
 dataErr <- sqrt(sum(dYData*dYData))/(length(wine_data_y))
+
+paste("using OLR the estimated test error = ",dataErr)
+#lm_wine_data
+summary(lm_wine_data)
+# QUESTION 2
+# Perform a ridge regression on the wine quality data set from problem 1 using only the first 1400 observations.
+#install.packages("glmnet")
+library(glmnet)
+#grid=10^seq(10,-2,length=100)
+wine_data<-read.csv("winequality-red.csv",header = TRUE, sep=";")
+
+ridge_wine_train = wine_data[1:1400,]
+ridge_x_wine_train = ridge_wine_train[,1:11]
+ridge_y_wine_train = ridge_wine_train[,12]
+
+test_wine_ridge = wine_data[1401:dim(scaled_wine_data)[1],]
+test_x_wine = test_wine_ridge[,1:11]
+test_y_wine = test_wine_ridge[,12]
+
+cv.out=cv.glmnet(as.matrix(ridge_x_wine_train), ridge_y_wine_train, alpha = 0 )
+plot(cv.out)
+bestlambda=cv.out$lambda.min
+bestlambda
+
+#Make fair comraison of Error
+ridgeMod=glmnet(as.matrix(ridge_x_wine_train), ridge_y_wine_train, alpha = 0, lambda = bestlambda)
+predictedQuality= predict(ridgeMod, newx = as.matrix(test_x_wine))
+ridge_error = sqrt(sum((test_y_wine - predictedQuality)^2))/length(predictedQuality)
+ridge_coef<-coef(ridgeMod)
+
+paste("using Ridge the estimated test error = ", ridge_error)
+paste("using Ridge Lambda = ", bestlambda, "alpha = 0")
+
 
